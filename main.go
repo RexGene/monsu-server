383d871121fcbeab11ec4f8142bccf492bd20462
RexGene <rexgene@hotmail.com>
2015-10-15 13:58:30 +0800

    账号修改

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	db := sqlproxy.NewSqlProxy("root", "1881982050~!@", "123.59.24.181", "3306", "game")
+	db := sqlproxy.NewSqlProxy("root", "123456", "111.59.24.181", "3306", "game")
 	err := db.Connect()
 	if err != nil {
 		panic(err)
